eedd37118b5ba7864d777a503a5b6304fc715f63
CRaLFa <minami.smd@gmail.com>
2024-11-12 13:34:06 +0900

    improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-. set_env.sh
+readonly BASE_DIR="$(realpath "$(dirname "$0")")"
 
 deploy_pub () {
 	(cd pub && go mod tidy)
@@ -15,7 +15,7 @@ deploy_pub () {
 		--trigger-http \
 		--allow-unauthenticated \
 		--env-vars-file=./.env.yaml \
-		--service-account="$SERVICE_ACCOUNT"
+		--service-account="$1"
 }
 
 deploy_sub () {
@@ -28,19 +28,22 @@ deploy_sub () {
 		--entry-point=Subscribe \
 		--trigger-topic=slack-gemini \
 		--env-vars-file=./.env.yaml \
-		--service-account="$SERVICE_ACCOUNT"
+		--service-account="$1"
 }
 
 main () {
-	cd "$(dirname "$0")"
+	cd "$BASE_DIR"
+	[ -r set_env.sh ] && . set_env.sh
+
 	[[ $# -lt 1 || "$1" = 'pub' ]] && {
 		echo -e 'Start deploying slack-gemini-pub function...\n'
-		deploy_pub
+		deploy_pub "$SERVICE_ACCOUNT"
 		echo
 	}
+
 	[[ $# -lt 1 || "$1" = 'sub' ]] && {
 		echo -e 'Start deploying slack-gemini-sub function...\n'
-		deploy_sub
+		deploy_sub "$SERVICE_ACCOUNT"
 	}
 }
 
